ee621785b8bd430d0c824fc2ffc9ccb19055523e
durjoy6405 <durjoy6405@gmail.com>
2023-10-30 00:10:44 +0600

    HTML & CSS done

---
+++ javascript/calculation.js
@@ -0,0 +1,10 @@
+function vatCalculation(total){
+    var v = total*.1;
+    return v;
+}
+
+const subTotal = document.getElementById("sub-total");
+subTotal.addEventListener("click",function(){
+
+})
+const totalVat = document.getElementById(total-vat);
